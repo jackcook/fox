5711b16e9f94651744616b2edad0005ff8cd79da
Jack Cook <hello@jackcook.nyc>
2015-08-15 15:05:18 -0700

    Create pay and sell fragments

---
+++ app/src/main/java/nyc/jackcook/fox/SellFragment.java
@@ -0,0 +1,33 @@
+package nyc.jackcook.fox;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import nyc.jackcook.fox.util.MainAdapter;
+
+public class SellFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sell, container, false);
+        return view;
+    }
+}
