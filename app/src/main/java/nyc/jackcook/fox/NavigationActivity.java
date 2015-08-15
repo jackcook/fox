7d4ce6e45021475110bd7a33e06ef68823b5f8cc
Jack Cook <hello@jackcook.nyc>
2015-08-15 13:19:39 -0700

    Changed toolbar text color

--- app/src/main/java/nyc/jackcook/fox/NavigationActivity.java
+++ app/src/main/java/nyc/jackcook/fox/NavigationActivity.java
@@ -1,14 +1,31 @@
 package nyc.jackcook.fox;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 import it.neokree.materialnavigationdrawer.elements.MaterialSection;
+import it.neokree.materialnavigationdrawer.elements.listeners.MaterialSectionListener;
+import it.neokree.materialnavigationdrawer.util.MaterialDrawerLayout;
 
 public class NavigationActivity extends MaterialNavigationDrawer {
 
+    private MainFragment mainFragment = null;
+    private TransactionsFragment transactionsFragment = null;
+
     @Override
     public void init(Bundle savedInstanceState) {
+//        MaterialSection home = newSection("Home", new MaterialSectionListener() {
+//            @Override
+//            public void onClick(MaterialSection materialSection) {
+//                changeToolbarColor(Color.parseColor("#ff8400"), Color.parseColor("#eb7a00"));
+//                setFragment(new MainFragment(), "Testing");
+//            }
+//        });
+
+//        getToolbar().setTitleTextColor();
+
         MaterialSection home = newSection("Home", new MainFragment());
         MaterialSection transactions = newSection("Transactions", new TransactionsFragment());
 
